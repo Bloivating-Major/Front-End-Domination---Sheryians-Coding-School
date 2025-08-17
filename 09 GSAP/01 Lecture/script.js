70b5ed380749dc0ce35f09aba8901bb8fce4fab0
Sambhav <sambhav12wakhariya@gmail.com>
2025-08-17 23:30:00 +0530

    GSAP Lecture 01 - Js Setup

---
+++ 09 GSAP/01 Lecture/script.js
@@ -0,0 +1,17 @@
+// gsap.to("#box", {
+//     x:900,
+//     duration: 2,
+//     delay:1,
+//     backgroundColor: "blue",
+//     rotate: 360,
+//     scale: 0.5
+// })
+
+gsap.from("#box", {
+    x:900,
+    duration: 2,
+    delay:1,
+    backgroundColor: "blue",
+    rotate: 360,
+    scale: 0.5
+})

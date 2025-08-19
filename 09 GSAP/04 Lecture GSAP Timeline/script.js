2b1553a1f08746b213ccbd4654d1cf0df91c1c0c
Sambhav <sambhav12wakhariya@gmail.com>
2025-08-19 23:30:00 +0530

    GSAP Lecture 04 Js Setup

---
+++ 09 GSAP/04 Lecture GSAP Timeline/script.js
@@ -0,0 +1,22 @@
+var t1 = gsap.timeline()
+
+t1.to("#box1" ,{
+    x : 700,
+    rotate: 360,
+    duration: 2,
+    scale : 0.5,
+})
+
+t1.to("#box2" ,{
+    x : 700,
+    rotate: 360,
+    duration: 2,
+    scale : 0.5,
+})
+
+t1.to("#box3" ,{
+    x : 700,
+    rotate: 360,
+    duration: 2,
+    scale : 0.5,
+})

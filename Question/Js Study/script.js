35c6cbf171210147371d01cc9f1495c1120883f3
Sambhav <sambhav12wakhariya@gmail.com>
2025-08-23 23:30:00 +0530

    JS Study

---
+++ Question/Js Study/script.js
@@ -0,0 +1,56 @@
+var Name = "Sambhav"
+
+let Age = 20
+const BloogGrp = " o +ve"
+Gender = 2
+let Value
+let Blank = null
+
+console.log(Name);
+console.log(Age);
+console.log(BloogGrp);
+console.log(Gender);
+// console.log(BirthDate);
+console.log(Value);
+console.log(Blank);
+console.log(typeof Gender);
+
+let num1 = 20
+let num2 = num1
+console.log(num1);
+console.log(num2);
+num2 = 25
+console.log(num2);
+console.log(num1);
+
+let A  = [1,2,3]
+let C  = A
+console.log(A);
+console.log(C);
+C.pop()
+console.log(C);
+console.log(A);
+
+let Arr = [1,2,3,4,5]
+let Arr2 = [...Arr]
+console.log(Arr);
+console.log(Arr2);
+Arr2.pop()
+console.log(Arr2);
+console.log(Arr);
+
+let Fruits = ["Apple", "Mango", "Grapes", "PineApple" , "Banana"]
+let Fruits2 = Fruits.slice(0,3);
+console.log(Fruits2);
+console.log(Fruits);
+
+let B = function(){}
+console.log(typeof B);
+console.log(B);
+
+
+
+
+
+
+
